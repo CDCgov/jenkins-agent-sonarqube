ecf5df2c9e5be1402d34076f143151a2b29573e6
Tim Taylor <ttaylor@mitre.org>
2018-11-01 17:14:37 -0400

    Fixed typo in env var

--- run-scan.sh
+++ run-scan.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sonar-scanner -Dsonar.host.url=$SONARQUBE_HOST_URL -Dsonar.projectKey=$SONARQUBE_PROJECT_KEY -Dsonar.projectName=$SONARUBE_PROJECT_NAME -Dsonar.sources=$SONARQUBE_SOURCE_DIR -Dsonar.login=$SONARQUBE_LOGIN_TOKEN
+sonar-scanner -Dsonar.host.url=$SONARQUBE_HOST_URL -Dsonar.projectKey=$SONARQUBE_PROJECT_KEY -Dsonar.projectName=$SONARQUBE_PROJECT_NAME -Dsonar.sources=$SONARQUBE_SOURCE_DIR -Dsonar.login=$SONARQUBE_LOGIN_TOKEN
